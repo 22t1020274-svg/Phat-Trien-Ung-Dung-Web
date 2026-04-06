8ae089b74bc0b92e22f8b49449c35b687ea2538a
22t1020274-svg <22t1020274@husc.edu.vn>
2026-04-07 00:14:00 +0700

    SV22T1010274.Admin

---
+++ Models/EmployeeRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SV22T1020274.Models.HR;
+
+
+namespace SV22T1020274.Admin.Models
+{
+    public class EmployeeRoleViewModel
+    {
+        public Employee Employee { get; set; } = new Employee();
+        public List<string> SelectedRoles { get; set; } = new List<string>();
+    }
+}
+
